0f01164f80c1cdf398f5373c115732eac16122ef
ExLuZiVe53 <imstudent27@gmail.com>
2023-09-17 13:49:46 +0200

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,23 +3,12 @@ import { Profile } from '../Profile/Profile.jsx';
 
 export const App = () => {
   return (
-    <div
-    // style={{
-    //   height: '100vh',
-    //   display: 'flex',
-    //   justifyContent: 'center',
-    //   alignItems: 'center',
-    //   fontSize: 40,
-    //   color: '#010101',
-    // }}
-    >
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-    </div>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,36 +1,79 @@
-// import css from './Profile.module.css';
+import styled from 'styled-components';
+import { Stats } from 'components/Stats/Stats';
+
+const StyledProfile = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledDescription = styled.div``;
+
+const StyledAvatar = styled.img`
+  width: 200px;
+`;
+
+const StyledName = styled.h1`
+  font-size: 24px;
+`;
+
+const StyledTag = styled.p`
+  font-size: 18px;
+  color: grey;
+`;
+
+const StyledLocation = styled.p`
+  font-size: 18px;
+  color: grey;
+`;
 
 export const Profile = props => {
-  const {
-    username,
-    tag,
-    location,
-    avatar,
-    stats: { followers, views, likes },
-  } = props;
   return (
-    <div className="profile">
-      <div className="description">
-        <img src={avatar} alt="User avatar" className="avatar" />
-        <p className="name">{username}</p>
-        <p className="tag">{tag}</p>
-        <p className="location">{location}</p>
-      </div>
-
-      <ul className="stats">
-        <li>
-          <span className="label">Followers</span>
-          <span className="quantity">{followers}</span>
-        </li>
-        <li>
-          <span className="label">Views</span>
-          <span className="quantity">{views}</span>
-        </li>
-        <li>
-          <span className="label">Likes</span>
-          <span className="quantity">{likes}</span>
-        </li>
-      </ul>
-    </div>
+    <StyledProfile className="profile">
+      <StyledDescription className="description">
+        <StyledAvatar src={props.avatar} alt="User avatar" className="avatar" />
+        <StyledName className="name">{props.username}</StyledName>
+        <StyledTag className="tag">{props.tag}</StyledTag>
+        <StyledLocation className="location">{props.location}</StyledLocation>
+      </StyledDescription>
+      <Stats />
+    </StyledProfile>
   );
 };
+
+// export const Profile = props => {
+//   const {
+//     username,
+//     tag,
+//     location,
+//     avatar,
+//     stats: { followers, views, likes },
+//   } = props;
+//   return (
+//     <div className="profile">
+//       <div className="description">
+//         <img src={avatar} alt="User avatar" className="avatar" />
+//         <p className="name">{username}</p>
+//         <p className="tag">{tag}</p>
+//         <p className="location">{location}</p>
+//       </div>
+
+//       <ul className="stats">
+//         <li>
+//           <span className="label">Followers</span>
+//           <span className="quantity">{followers}</span>
+//         </li>
+//         <li>
+//           <span className="label">Views</span>
+//           <span className="quantity">{views}</span>
+//         </li>
+//         <li>
+//           <span className="label">Likes</span>
+//           <span className="quantity">{likes}</span>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// };
